b4ed6cb6a95808c07a6902ed7264a0e62962906b
Ваня Конюх <vkonyuh956@Mail.ru>
2022-12-18 22:05:20 +0500

    123

--- SalaryApp/Аccountant.xaml.cs
+++ SalaryApp/Аccountant.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,29 @@ namespace SalaryApp
     /// </summary>
     public partial class Аccountant : Window
     {
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=DESKTOP-QCVCABK\\SQLEXPRESS;Trusted_Connection=Yes;DataBase=salary_db;");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+
         public Аccountant()
         {
             InitializeComponent();
+        }
+
+        private void Award_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            Award.Items.Add("sdsd");
         }
     }
 }
