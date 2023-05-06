709aaa9562f74e278e5b9338e56100880089b91b
Your Name <you@example.com>
2023-05-06 20:32:23 +0500

    map

---
+++ SalaryApp/Windows/Map/TestMap.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SalaryApp.Windows.Map
+{
+    /// <summary>
+    /// Логика взаимодействия для TestMap.xaml
+    /// </summary>
+    public partial class TestMap : Window
+    {
+        public TestMap()
+        {
+            InitializeComponent();
+
+            MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
+            MainMapView.SetViewpoint(new Viewpoint(mapCenterPoint, 100000));
+        }
+    }
+}
